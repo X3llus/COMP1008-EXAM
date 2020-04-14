db20d2b1309266f71f651732aebc2b6cdf7cd2e4
Braden Coates <xellus@pop-os.localdomain>
2020-04-14 15:08:30 -0400

    7

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,27 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 public class Controller {
 
-    @FXML
+    @FXML private RadioButton fiction;
+    @FXML private RadioButton nonFiction;
+    @FXML private ListView<Author> authors;
+    @FXML private ListView<String> genres;
+    @FXML private ListView<Book> books;
     @FXML private void initialize() {
         ToggleGroup group = new ToggleGroup();
-        radioButton.setToggleGroup(group);
-        radioButton2.setToggleGroup(group);
+        fiction.setToggleGroup(group);
+        nonFiction.setToggleGroup(group);
+
+        authors.getItems().addAll(Arrays.asList(new Author("Braden", "Coates", LocalDate.of(2001, 1, 1)),
+                new Author("John", "Doe", LocalDate.of(1989, 4, 17)),
+                new Author ("Randy", "Linx", LocalDate.of(1567, 11, 26))));
     }
 }
